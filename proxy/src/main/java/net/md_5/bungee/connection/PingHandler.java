4221b97e97cca1d150ab7b2bfc01830d03f7df0a
Myles <mylescomputer@gmail.com>
2014-11-01 13:57:39 +0000

    WARNING: This commit removes sending Animated MOTDs, due to a bug with the way Bungee works it causes players to randomly disconnect so this removes it. If you wish to take the risk, role this commit back and use that.

--- proxy/src/main/java/net/md_5/bungee/connection/PingHandler.java
+++ proxy/src/main/java/net/md_5/bungee/connection/PingHandler.java
@@ -23,45 +23,44 @@ import net.md_5.bungee.protocol.packet.StatusResponse;
 public class PingHandler extends PacketHandler
 {
 
-    private final ServerInfo target;
-    private final Callback<ServerPing> callback;
-    private final int protocol;
-    private ChannelWrapper channel;
+	private final ServerInfo target;
+	private final Callback<ServerPing> callback;
+	private final int protocol;
+	private ChannelWrapper channel;
 
-    @Override
-    public void connected(ChannelWrapper channel) throws Exception
-    {
-        this.channel = channel;
-        MinecraftEncoder encoder = new MinecraftEncoder( Protocol.HANDSHAKE, false, protocol );
+	@Override
+	public void connected(ChannelWrapper channel) throws Exception
+	{
+		this.channel = channel;
+		MinecraftEncoder encoder = new MinecraftEncoder( Protocol.HANDSHAKE, false, protocol );
 
-        channel.getHandle().pipeline().addAfter( PipelineUtils.FRAME_DECODER, PipelineUtils.PACKET_DECODER, new MinecraftDecoder( Protocol.STATUS, false, ProxyServer.getInstance().getProtocolVersion() ) );
-        channel.getHandle().pipeline().addAfter( PipelineUtils.FRAME_PREPENDER, PipelineUtils.PACKET_ENCODER, encoder );
+		channel.getHandle().pipeline().addAfter( PipelineUtils.FRAME_DECODER, PipelineUtils.PACKET_DECODER, new MinecraftDecoder( Protocol.STATUS, false, ProxyServer.getInstance().getProtocolVersion() ) );
+		channel.getHandle().pipeline().addAfter( PipelineUtils.FRAME_PREPENDER, PipelineUtils.PACKET_ENCODER, encoder );
 
-        channel.write( new Handshake( protocol, target.getAddress().getHostString(), target.getAddress().getPort(), 1 ) );
+		channel.write( new Handshake( protocol, target.getAddress().getHostString(), target.getAddress().getPort(), 1 ) );
 
-        encoder.setProtocol( Protocol.STATUS );
-        channel.write( new StatusRequest() );
-    }
+		encoder.setProtocol( Protocol.STATUS );
+		channel.write( new StatusRequest() );
+	}
 
-    @Override
-    public void exception(Throwable t) throws Exception
-    {
-        callback.done( null, t );
-    }
+	@Override
+	public void exception(Throwable t) throws Exception
+	{
+		callback.done( null, t );
+	}
 
-    @Override
-    @SuppressFBWarnings("UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR")
-    public void handle(StatusResponse statusResponse) throws Exception
-    {
-        Gson gson = protocol == ProtocolConstants.MINECRAFT_1_7_2 ? BungeeCord.getInstance().gsonLegacy : BungeeCord.getInstance().gson;
-        callback.done( gson.fromJson( statusResponse.getResponse(), ServerPing.class ), null );
-     	//  channel.close();
-		// REMOVED, this will allow us to animate the server-icon
-    }
+	@Override
+	@SuppressFBWarnings("UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR")
+	public void handle(StatusResponse statusResponse) throws Exception
+	{
+		Gson gson = protocol == ProtocolConstants.MINECRAFT_1_7_2 ? BungeeCord.getInstance().gsonLegacy : BungeeCord.getInstance().gson;
+		callback.done( gson.fromJson( statusResponse.getResponse(), ServerPing.class ), null );
+		channel.close();
+	}
 
-    @Override
-    public String toString()
-    {
-        return "[Ping Handler] -> " + target.getName();
-    }
+	@Override
+	public String toString()
+	{
+		return "[Ping Handler] -> " + target.getName();
+	}
 }
